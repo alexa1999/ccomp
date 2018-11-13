63b13020e8e3b4d89c517b41173cdee07e0c5220
alexa1999 <anel.veliz@ucsp.edu.pe>
2018-11-13 08:54:27 -0500

    Update main.cpp

--- arrayPol/main.cpp
+++ arrayPol/main.cpp
@@ -10,7 +10,10 @@ int main()
     Rectangle r1(2,3);
     Polygon *p=&r1;
 
-
+    Rectangle r2(4,5);
+    Polygon *p=&r1;
+    Rectangle r3(2,3);
+    Polygon *p=&r1;
 
     Triangle t1(2,3);
     Polygon *pt=&t1;
@@ -23,6 +26,11 @@ int main()
 //    PolyArray p1(arr,size);
 
 
+    Rectangle arr[]={a,b};
+    int size =sizeof(arr)/sizeof(arr[0]);
+    PolyArray pa(arr,size);
+    pa.push_back(r3);
+    pa.print()
 
     //Polygon *p1=&p;
 
